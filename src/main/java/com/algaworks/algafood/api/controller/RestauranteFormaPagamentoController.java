41017ad65241b02836f6331f368c9f5ee4a391f6
Alex Holanda <alex.holanda@outlook.com>
2021-05-28 14:44:15 -0300

    Documentado a endpoint de restaurante-forma-pagamento

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
+import com.algaworks.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenAPI;
 import com.algaworks.algafood.domain.service.RestauranteFormaPagamentoService;
 
 @RestController
 @RequestMapping(path = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
-public class RestauranteFormaPagamentoController {
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenAPI {
 
 	@Autowired
 	private FormaPagamentoModelAssembler formaPagamentoModelAssembler;
@@ -26,6 +27,7 @@ public class RestauranteFormaPagamentoController {
 	@Autowired
 	private RestauranteFormaPagamentoService restauranteFormaPagamentoService;
 	
+	@Override
 	@GetMapping
 	public ResponseEntity<List<FormaPagamentoModel>> listar(@PathVariable Long restauranteId) {
 		var formasPagamento = restauranteFormaPagamentoService.listar(restauranteId);
@@ -33,12 +35,14 @@ public class RestauranteFormaPagamentoController {
 		return ResponseEntity.ok(formasPagamentoModel);
 	}
 	
+	@Override
 	@PutMapping("/{formaPagamentoId}")
 	public ResponseEntity<Void> associarFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
 		restauranteFormaPagamentoService.associarFormaPagamento(restauranteId, formaPagamentoId);
 		return ResponseEntity.noContent().build();
 	}
 	
+	@Override
 	@DeleteMapping("/{formaPagamentoId}")
 	public ResponseEntity<Void> desassociarFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
 		restauranteFormaPagamentoService.desassociarFormaPagamento(restauranteId, formaPagamentoId);
---
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteFormaPagamentoControllerOpenAPI.java
@@ -0,0 +1,66 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.algaworks.algafood.api.model.FormaPagamentoModel;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+public interface RestauranteFormaPagamentoControllerOpenAPI {
+
+	@Operation(summary = "Listar formas de pagamento", description = "Lista as formas de pagamento associadas", tags = { "Restaurantes" })
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Sucesso na listagem das formas de pagamento", 
+				content = @Content(array = @ArraySchema(schema = @Schema(implementation = FormaPagamentoModel.class)))),
+			@ApiResponse(responseCode = "500", description = "Erro Interno no Servidor", 
+				content = @Content(schema = @Schema(implementation = Problem.class))) 
+	})
+	public ResponseEntity<List<FormaPagamentoModel>> listar(
+			@Parameter(description = "ID do restaurante", example = "1", required = true)
+			Long restauranteId);
+	
+	@Operation(summary = "Associar forma de pagamento", description = "Associa uma forma de pagamento ao restaurante", 
+				tags = { "Restaurantes" })
+	@ApiResponses({
+		@ApiResponse(responseCode = "204", description = "Associa forma de pagamento ao restaurante", 
+				content = @Content),
+		@ApiResponse(responseCode = "400", description = "Problema no recurso", 
+				content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(responseCode = "404", description = "Recurso não encontrado", 
+				content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(responseCode = "500", description = "Erro Interno no Servidor", 
+				content = @Content(schema = @Schema(implementation = Problem.class)))
+	})
+	public ResponseEntity<Void> associarFormaPagamento(
+			@Parameter(description = "ID do restaurante", example = "1", required = true)
+			Long restauranteId, 
+			@Parameter(description = "ID da forma de pagamento", example = "1", required = true)
+			Long formaPagamentoId);
+	
+	@Operation(summary = "Associar forma de pagamento", description = "Associa uma forma de pagamento ao restaurante", 
+				tags = { "Restaurantes" })
+	@ApiResponses({
+		@ApiResponse(responseCode = "204", description = "Desassociar forma de pagamento do restaurante", 
+				content = @Content),
+		@ApiResponse(responseCode = "400", description = "Problema no recurso", 
+				content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(responseCode = "404", description = "Recurso não encontrado", 
+				content = @Content(schema = @Schema(implementation = Problem.class))),
+		@ApiResponse(responseCode = "500", description = "Erro Interno no Servidor", 
+				content = @Content(schema = @Schema(implementation = Problem.class)))
+	})
+	public ResponseEntity<Void> desassociarFormaPagamento(
+			@Parameter(description = "ID do restaurante", example = "1", required = true)
+			Long restauranteId, 
+			@Parameter(description = "ID da forma de pagamento", example = "1", required = true)
+			Long formaPagamentoId);
+}
