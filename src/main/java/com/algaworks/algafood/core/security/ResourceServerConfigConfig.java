89fd0584b8c983e66825b3d2af97e4694cd480b5
Alex Holanda <alex.holanda@outlook.com>
2021-06-08 16:05:18 -0300

    Configurado Resource Server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfigConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfigConfig.java
@@ -10,6 +10,9 @@ public class ResourceServerConfigConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated()
-			.and().oauth2ResourceServer().opaqueToken();
+			.and()
+				.cors()
+			.and()
+				.oauth2ResourceServer().opaqueToken();
 	}
 }
