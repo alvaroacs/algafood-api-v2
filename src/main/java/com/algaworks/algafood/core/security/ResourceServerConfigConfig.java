b5382981aeb05aff9dc14441e05f153625a7fda1
Alex Holanda <alex.holanda@outlook.com>
2021-06-09 15:22:53 -0300

    configurado chave publica para validar jwt

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfigConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfigConfig.java
@@ -1,23 +1,11 @@
 package com.algaworks.algafood.core.security;
 
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @EnableWebSecurity
 public class ResourceServerConfigConfig extends WebSecurityConfigurerAdapter {
-
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec("47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU".getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
