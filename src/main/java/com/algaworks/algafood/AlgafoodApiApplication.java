e8de0bb046e2d13835528e1d8d3fa2ad6a3126bb
Alex Holanda <alex.holanda@outlook.com>
2021-06-12 14:53:07 -0300

    externalizado o keystore, criando um protocolresolver para base64

--- src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -6,11 +6,18 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.algaworks.algafood.core.io.Base64ProtocolResolver;
+
 @SpringBootApplication
 public class AlgafoodApiApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
-		SpringApplication.run(AlgafoodApiApplication.class, args);
+		
+		var app = new SpringApplication(AlgafoodApiApplication.class);
+		app.addListeners(new Base64ProtocolResolver());
+		
+		app.run(args);
+//		SpringApplication.run(AlgafoodApiApplication.class, args);
 	}
 }
