942b4c6227961a16c1c15cb9def2dad0bbc8d396
Alex Holanda <alex.holanda@outlook.com>
2021-06-14 20:07:57 -0300

    configurado o arquivo de docker compose e adicionado o wait-for-it para quando terminar o bd começar a criar o container do app

--- src/test/java/com/algaworks/algafoodapi/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafoodapi/AlgafoodApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.algaworks.algafoodapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlgafoodApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
