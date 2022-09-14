480f7d3d487659d9462e5c2dcce1147ad83e93a4
Alex Holanda <alex.holanda@outlook.com>
2022-09-14 20:29:33 -0300

    Criado o AuthorizationServerConfig

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -7,12 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -27,31 +30,31 @@ public class AuthorizationServerConfig {
 	@Order(Ordered.HIGHEST_PRECEDENCE)
 	public SecurityFilterChain authFilterChain(HttpSecurity http) throws Exception {
 		OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-		
+
 		return http.build();
 	}
-	
+
 	@Bean
 	public ProviderSettings providerSettings(AlgafoodSecurityProperties properties) {
 		return ProviderSettings.builder().issuer(properties.getProviderUrl()).build();
 	}
-	
+
 	@Bean
 	public RegisteredClientRepository clientRepository(PasswordEncoder passwordEncoder) {
-		RegisteredClient algafoodBackend = RegisteredClient
-				.withId("1")
-				.clientId("algafood-backend")
+		RegisteredClient algafoodBackend = RegisteredClient.withId("1").clientId("algafood-backend")
 				.clientSecret(passwordEncoder.encode("backend123"))
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-				.scope("read")
-				.tokenSettings(TokenSettings.builder()
-						.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
-						.accessTokenTimeToLive(Duration.ofMinutes(30))
-						.build())
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS).scope("READ")
+				.tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+						.accessTokenTimeToLive(Duration.ofMinutes(30)).build())
 				.build();
-				
- 		
+
 		return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodBackend));
+	}
+
+	@Bean
+	public OAuth2AuthorizationService auth2AuthorizationService(JdbcOperations jdbcOperations,
+			RegisteredClientRepository registeredClientRepository) {
+		return new JdbcOAuth2AuthorizationService(jdbcOperations, registeredClientRepository);
 	}
 }
